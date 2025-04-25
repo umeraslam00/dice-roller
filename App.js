450fae26ffbc6fbce9782dc80d862a365c475cfe
umeraslam00 <joreypull@gmail.com>
2025-04-25 15:10:05 +0500

    v1.0

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [dice, setDice] = useState(1);
+
+  const rollDice = () => {
+    const result = Math.floor(Math.random() * 6) + 1;
+    setDice(result);
+  }
+
   return (
+    // View is like a <div> in React JS
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Header text */}
+      <Text style={styles.title}>🎲 Dice Roller</Text>
+
+      {/* This displays the current dice number */}
+      <Text style={styles.diceNumber}>{dice}</Text>
+
+      {/* TouchableOpacity for a custom button */}
+      <TouchableOpacity onPress={rollDice} style={styles.diceBtn}>
+        <Text style={styles.btnTxt}>Roll the Dice</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.credits}>Developed by: Umer Aslam</Text>
+      
     </View>
   );
 }
@@ -17,4 +39,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  diceNumber: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    color: '#333',
+    marginVertical: 30,
+  },
+
+  title: {
+    fontSize: 40,
+    marginBottom: 15,
+    fontWeight: '600',
+  },
+
+  btnTxt: {
+    fontSize: 25,
+    color: '#fff',
+    fontWeight: '600',
+  },
+
+  diceBtn: {
+    width: 220, // fixed width
+    height: 70, // custom height
+    backgroundColor: '#050303', // background color
+    justifyContent: 'center', // centers text vertically
+    alignItems: 'center', // centers text horizontally
+    borderRadius: 25, // rounded corners
+  },
+
+  credits: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#160404',
+    position: 'absolute',
+    bottom: 20,
+  }
+
+
 });
